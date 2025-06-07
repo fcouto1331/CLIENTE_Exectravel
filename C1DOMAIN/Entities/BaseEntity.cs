48f0f4db5d5fbd84698703f3fdaf80454cadf6e2
Flávio Couto <flavio.couto@live.com>
2025-06-07 12:15:55 -0300

    TASK - Início

---
+++ C1DOMAIN/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace C1DOMAIN.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity(int id, Guid guidId)
+        {
+            Id = id;
+            GuidId = guidId;
+        }
+
+        public int Id { get; private set; }
+        public Guid GuidId { get; private set; }
+    }
+}
---
+++ C1DOMAIN/Entities/EmpresaEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace C1DOMAIN.Entities
+{
+    public class EmpresaEntity : BaseEntity
+	{
+		public EmpresaEntity(int id, Guid guidId, bool empresaStatus, DateTime empresaDataCadastro, string nome)
+			: base(id, guidId)
+		{
+			EmpresaStatus = empresaStatus;
+			EmpresaDataCadastro = empresaDataCadastro;
+			Nome = nome;
+		}
+		public bool EmpresaStatus { get; private set; }
+		public DateTime EmpresaDataCadastro { get; private set; }
+		public string Nome { get; private set; }
+    }
+}
